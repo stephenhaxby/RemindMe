883bcf8b61e3df057f37a111d78a3f1481cde358
Stephen Haxby <stephen.haxby@me.com>
2015-12-15 21:14:11 +1100

    Updates to the edit UI...

--- RemindMe/RemindMeTableViewCell.swift
+++ RemindMe/RemindMeTableViewCell.swift
@@ -24,6 +24,23 @@ class RemindMeTableViewCell: UITableViewCell {
 
                 reminderTextLabel.text = itemReminder.title
                 
+                let originalReminderText : NSString = NSString(string: reminderTextLabel.text!)
+                
+                var reminderText : NSString = NSString(string: reminderTextLabel.text!)
+                
+                truncateText(reminderText, forLabel: reminderTextLabel)
+                
+                var newReminderText = String(reminderText)
+                
+                if originalReminderText.length > reminderText.length {
+                    
+                    reminderText = reminderText.substringWithRange(NSRange(location: 0, length: reminderText.length-3))
+                    
+                    newReminderText.appendContentsOf("...")
+                }
+                
+                reminderTextLabel.text = newReminderText
+                
                 if let dueDateComponents = itemReminder.dueDateComponents {
 
                     reminderTimeLable.text = NSDateManager.dateStringFromComponents(dueDateComponents)
@@ -31,6 +48,20 @@ class RemindMeTableViewCell: UITableViewCell {
                 
                 setupCellVisibilityFor(itemReminder)
             }
+        }
+    }
+    
+    func truncateText(var text : NSString, forLabel : UILabel) {
+        
+        let size : CGSize = text.sizeWithAttributes([NSFontAttributeName : reminderTextLabel.font!])
+        
+        let labelWidth = forLabel.bounds.size.width
+        
+        if size.width > labelWidth {
+            
+            text = text.substringWithRange(NSRange(location: 0, length: text.length-1))
+            
+            truncateText(text, forLabel: forLabel)
         }
     }
     
