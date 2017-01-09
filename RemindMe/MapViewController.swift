632ae933cc5c3482ec1ba69dce5460c71adecb9f
Stephen Haxby <stephenhaxby@stephens-mac-mini.local>
2017-01-10 06:09:23 +1100

    Adding Map search...

--- RemindMe/MapViewController.swift
+++ RemindMe/MapViewController.swift
@@ -8,7 +8,7 @@
 
 import MapKit
 
-class MapViewController : UIViewController, UIGestureRecognizerDelegate {
+class MapViewController : UIViewController, UIGestureRecognizerDelegate, UISearchBarDelegate {
     
     var settingsTableViewCell : SettingsTableViewCell?
     
@@ -19,13 +19,14 @@ class MapViewController : UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     @IBOutlet weak var map: MKMapView!
     
     required init?(coder aDecoder: NSCoder) {
         
         super.init(coder: aDecoder)
     }
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,13 +37,21 @@ class MapViewController : UIViewController, UIGestureRecognizerDelegate {
             displayLocation(forLatitude: setting!.latitude, andLongitude: setting!.longitude, withDefaultZoom: true)
         }
         
-        let gestureRecognizer: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.addAnnotation(_:)))
+        let longPressGestureRecognizer : UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.addAnnotation(_:)))
+        longPressGestureRecognizer.delegate = self
+        longPressGestureRecognizer.minimumPressDuration = 0.25
+        longPressGestureRecognizer.numberOfTouchesRequired = 1
+        map.addGestureRecognizer(longPressGestureRecognizer)
         
-        gestureRecognizer.delegate = self
-        gestureRecognizer.minimumPressDuration = 0.25
-        gestureRecognizer.numberOfTouchesRequired = 1
+        let panGestureRecognizer : UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(MapViewController.dismissSearch))
+        panGestureRecognizer.delegate = self
+        map.addGestureRecognizer(panGestureRecognizer)
         
-        map.addGestureRecognizer(gestureRecognizer)
+        let tapGestureRecognizer : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(MapViewController.dismissSearch))
+        tapGestureRecognizer.delegate = self
+        map.addGestureRecognizer(tapGestureRecognizer)
+        
+        searchBar.delegate = self
     }
     
     override func viewWillDisappear(_ animated : Bool){
@@ -64,6 +73,45 @@ class MapViewController : UIViewController, UIGestureRecognizerDelegate {
     override func viewDidDisappear(_ animated: Bool) {
         
         settingsTableViewCell?.displayLocation(forLatitude: setting!.latitude, andLongitude: setting!.longitude)
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+     
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        map.removeAnnotations(map.annotations as [MKAnnotation])
+
+        let localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        
+        let localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { (localSearchResponse, error) -> Void in
+            
+            searchBar.resignFirstResponder()
+            
+            let location = CLLocationCoordinate2DMake(localSearchResponse!.boundingRegion.center.latitude, localSearchResponse!.boundingRegion.center.longitude)
+
+            self.displayLocation(forLatitude: location.latitude, andLongitude: location.longitude, withDefaultZoom: true)
+            
+            
+//            if localSearchResponse == nil{
+//                let alertController = UIAlertController(title: nil, message: "Place Not Found", preferredStyle: UIAlertControllerStyle.alert)
+//                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+//                self.present(alertController, animated: true, completion: nil)
+//                return
+//            }
+
+//            self.pointAnnotation = MKPointAnnotation()
+//            self.pointAnnotation.title = searchBar.text
+//            self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude:     localSearchResponse!.boundingRegion.center.longitude)
+            
+//            self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+//            self.mapView.centerCoordinate = self.pointAnnotation.coordinate
+//            self.mapView.addAnnotation(self.pinAnnotationView.annotation!)
+        }
     }
     
     func addAnnotation(_ gestureRecognizer:UIGestureRecognizer) {
@@ -98,4 +146,12 @@ class MapViewController : UIViewController, UIGestureRecognizerDelegate {
         }
     }
 
+    func dismissSearch(gestureRecognizer: UIGestureRecognizer){
+        
+        if (gestureRecognizer.state == UIGestureRecognizerState.began
+            || gestureRecognizer.state == UIGestureRecognizerState.ended) {
+        
+            searchBar.resignFirstResponder()
+        }
+    }
 }
